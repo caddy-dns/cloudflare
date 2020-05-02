2d0180ea97cd908f7bb6f52da4028af63af7c69b
Matthew Holt <mholt@users.noreply.github.com>
2020-05-02 17:28:14 -0600

    Unmarshal Caddyfile
    Also allocate underlying Provider value, very important. Could also do
    this in Provision() but I figured this is the point of the New()
    function, especially since our type is supposed to be basically a
    transparent wrapper.

--- cloudflare.go
+++ cloudflare.go
@@ -2,6 +2,7 @@ package cloudflare
 
 import (
 	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/libdns/cloudflare"
 )
 
@@ -16,6 +17,45 @@ func init() {
 func (Provider) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
 		ID:  "dns.providers.cloudflare",
-		New: func() caddy.Module { return new(Provider) },
+		New: func() caddy.Module { return &Provider{new(cloudflare.Provider)} },
 	}
 }
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+// cloudflare [<api_token>] {
+//     api_token <api_token>
+// }
+//
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	repl := caddy.NewReplacer()
+	for d.Next() {
+		if d.NextArg() {
+			p.Provider.APIToken = repl.ReplaceAll(d.Val(), "")
+		}
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "api_token":
+				if p.Provider.APIToken != "" {
+					return d.Err("API token already set")
+				}
+				p.Provider.APIToken = repl.ReplaceAll(d.Val(), "")
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	if p.Provider.APIToken == "" {
+		return d.Err("missing API token")
+	}
+	return nil
+}
+
+// Interface guard
+var _ caddyfile.Unmarshaler = (*Provider)(nil)
