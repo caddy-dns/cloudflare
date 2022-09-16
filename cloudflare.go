815abbf88b27182428c342b2916a37b7134d266b
Matthew Holt <mholt@users.noreply.github.com>
2022-09-16 08:29:52 -0600

    Add license

--- cloudflare.go
+++ cloudflare.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Matthew Holt
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cloudflare
 
 import (
@@ -30,9 +44,9 @@ func (p *Provider) Provision(ctx caddy.Context) error {
 
 // UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
 //
-// cloudflare [<api_token>] {
-//     api_token <api_token>
-// }
+//	cloudflare [<api_token>] {
+//	    api_token <api_token>
+//	}
 //
 // Expansion of placeholders in the API token is left to the JSON config caddy.Provisioner (above).
 func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
